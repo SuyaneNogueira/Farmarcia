29659daabfd2a2a3b4e44b938b466a872564d491
SuyaneNogueira <naolhenaofale7@gmail.com>
2024-09-26 17:29:39 -0300

    FarMarcia

---
+++ Farmarcia/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import FarMarcia from './Farmarcia/FarMarcia'
+
+function App() {
+
+
+  return (
+    <>
+      <FarMarcia/>
+    </>
+  )
+}
+
+export default App
---
+++ Farmarcia/src/Farmarcia/FarMarcia.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+function FarMarcia() {
+  return (
+    <div>
+       
+    <div>
+      <header>
+        <img src="public/farmarcia.PNG.png" alt=""/>
+
+        <div>
+          <ul class="itens">
+              <li><a class="link1" href="#">Estoque</a></li>
+              <li><a class="link2" href="#">Atendiemento</a></li>
+              <li><a class="link3" href="#">vendas</a></li>
+              <li><a class="link4" href="#">Contado</a></li>
+          </ul>
+      </div>
+      </header>
+    </div>
+
+     <center> <div class="SOBN">
+        <h3 >SOB NOVA DIREÇÃO</h3>
+
+       <img src="public/farmarcia.PNG.png" alt="" />
+      </div>
+      </center>
+    </div>
+  )
+}
+
+export default FarMarcia
